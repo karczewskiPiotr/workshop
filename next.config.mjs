42735f813874a8642a2137ec3041421e70d9c3cd
Piotr Karczewski <p.karczewsk@gmail.com>
2024-03-13 15:07:28 +0000

    feat: configure t3-env for type-safe env variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,11 @@
+import { fileURLToPath } from "node:url";
+import createJiti from "jiti";
+
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+// Import env here to validate during build.
+jiti("./src/env");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
---
+++ src/env.ts
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { vercel } from "@t3-oss/env-nextjs/presets";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+  },
+  shared: {
+    NODE_ENV: z.enum(["development", "production"]),
+  },
+  experimental__runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+  },
+  extends: [vercel],
+});
