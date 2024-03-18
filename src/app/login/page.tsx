99e380b9c023301cf016f98720955ca09f9370fa
karczewskiPiotr <p.karczewsk@gmail.com>
2024-03-18 18:44:50 +0100

    feat: redirect unauthenticated users to login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,11 @@
+import validateRequest from "@/api/validateRequest";
 import SignInForm from "./_components/Login";
+import { redirect } from "next/navigation";
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  const { user } = await validateRequest();
+  if (user) return redirect(user.role === "admin" ? "/users" : "/");
+
   return (
     <main className="space-y-4">
       <h1>Sign In</h1>
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,6 +1,11 @@
+import validateRequest from "@/api/validateRequest";
 import SignUpForm from "./_components/SignUpForm";
+import { redirect } from "next/navigation";
 
-export default function SignUpPage() {
+export default async function SignUpPage() {
+  const { user } = await validateRequest();
+  if (user) return redirect(user.role === "admin" ? "/users" : "/");
+
   return (
     <main className="space-y-4">
       <h1>Request an account</h1>
