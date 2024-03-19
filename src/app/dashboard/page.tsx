9d9a58d9e4fda6e883518899174a198e2fca4ff0
Piotr Karczewski <p.karczewsk@gmail.com>
2024-03-19 15:26:27 +0100

    feat: guard dashboard from unauthenticated users

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,10 @@
-export default function DashboardPage() {
+import validateRequest from "@/api/auth/validateRequest";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const { user } = await validateRequest();
+  if (!user) return redirect("/login");
+
   return (
     <main>
       <h1>Dashboard</h1>
