39c63e2172a545fce663ee8e7c7707860331eec5
Piotr Karczewski <p.karczewsk@gmail.com>
2024-04-23 15:45:17 +0200

    feat: improve login page
    - add client side validation to the form
    - style the form
    - add link to sign in
    - futureproof password reset

--- src/app/(auth)/login/_components/login-form.tsx
+++ src/app/(auth)/login/_components/login-form.tsx
@@ -2,28 +2,110 @@
 
 import login from "@/api/auth/login";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { insertUserSchema, selectUserSchema } from "@/db/schema";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { FormEvent, useRef } from "react";
 import { useFormState } from "react-dom";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = insertUserSchema.pick({
+  email: true,
+  password: true,
+});
 
 export default function SignUpForm() {
+  const formRef = useRef<HTMLFormElement>(null);
   const [state, formAction] = useFormState(login, { errors: [] });
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    mode: "onBlur",
+  });
+
+  function submitForm(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    form.handleSubmit(() => formAction(new FormData(formRef.current!)))(event);
+  }
+
   return (
-    <form className="space-y-2" action={formAction}>
-      <Label htmlFor="email">Email</Label>
-      <Input name="email" id="email" type="text" />
-      <br />
-      <Label htmlFor="password">Password</Label>
-      <Input type="password" name="password" id="password" />
-      <br />
-      {state?.errors.map((error, index) => (
-        <p key={`loginFormError-${index}`} className="text-destructive">
-          {error}
-        </p>
-      ))}
-      <br />
-      <Button type="submit">Continue</Button>
-    </form>
+    <>
+      <Form {...form}>
+        <form
+          ref={formRef}
+          action={formAction}
+          onSubmit={submitForm}
+          className="grid gap-4"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="grid gap-2">
+                <FormLabel asChild>
+                  <Label>Email</Label>
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className="grid gap-2">
+                <div className="flex items-center">
+                  <FormLabel asChild>
+                    <Label>Password</Label>
+                  </FormLabel>
+                  <Link
+                    href="#"
+                    className="ml-auto inline-block text-sm underline pointer-events-none"
+                  >
+                    Forgot your password?
+                  </Link>
+                </div>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className={cn(state.errors.length === 0 && "hidden")}>
+            {state?.errors.map((error, index) => (
+              <p key={`loginFormError-${index}`} className="text-destructive">
+                {error}
+              </p>
+            ))}
+          </div>
+
+          <Button type="submit">Log in</Button>
+        </form>
+      </Form>
+
+      <div className="mt-4 text-center text-sm">
+        Don&apos;t have an account?{" "}
+        <Link href="/sign-up" className="underline">
+          Sign up
+        </Link>
+      </div>
+    </>
   );
 }
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,13 @@
 import validateRequest from "@/api/auth/validate-request";
 import SignInForm from "./_components/login-form";
 import { redirect } from "next/navigation";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default async function SignInPage() {
   const { user } = await validateRequest();
@@ -8,9 +15,18 @@ export default async function SignInPage() {
   else if (user) return redirect("/dashboard");
 
   return (
-    <main className="space-y-4 px-2">
-      <h1>Log In</h1>
-      <SignInForm />
+    <main className="flex justify-center items-center flex-auto">
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl">Log in</CardTitle>
+          <CardDescription>
+            Enter your email below to log in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInForm />
+        </CardContent>
+      </Card>
     </main>
   );
 }
