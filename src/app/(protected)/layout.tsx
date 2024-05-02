b911b4450453ef81070c37924593bec83ead4aa4
Piotr Karczewski <p.karczewsk@gmail.com>
2024-05-02 14:56:19 +0200

    refactor: cleanup ProtectedLayout component file

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,29 +1,6 @@
 import validateRequest from "@/api/auth/validate-request";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { Hexagon, Home, PanelLeft, Settings } from "lucide-react";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 import React, { PropsWithChildren } from "react";
-
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Button } from "@/components/ui/button";
-
-import { ModeToggle } from "@/components/mode-toggle";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import UserDropdown from "./dashboard/_components/test2";
-import Dashboard from "@/components/dashboard";
 
 type Props = Readonly<PropsWithChildren<{ navigation: React.ReactNode }>>;
 
