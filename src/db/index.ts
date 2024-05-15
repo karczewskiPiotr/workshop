01f00e0025c770c2dcdff8487a3746957c403a9d
Piotr Karczewski <p.karczewsk@gmail.com>
2024-05-15 14:37:59 +0100

    fix: use serverless neon driver

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,14 @@
 import { env } from "../env";
-import { NeonQueryFunction, neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 
 // Use local db wtih posthres-js adapter in development
 import { drizzle as drizzleDev } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 // Use Neon serverless db in production
-const sql = neon(env.DATABASE_URL) as NeonQueryFunction<boolean, boolean>;
-const sqlDev = postgres(env.DATABASE_URL, { idle_timeout: 300 });
+const pool = new Pool({ connectionString: env.DATABASE_URL });
+const sql = postgres(env.DATABASE_URL, { idle_timeout: 300 });
 
 export const db =
-  env.NODE_ENV === "production" ? drizzle(sql) : drizzleDev(sqlDev);
+  env.NODE_ENV === "production" ? drizzle(pool) : drizzleDev(sql);
